f4c27fce01a12ef7fdea594f633f5b9306e3aa08
Jade Hamel <jhamel@cognibox.com>
2021-08-30 23:22:35 -0400

    Add features in Amazonca class. return title and description excluding amazon mentions. Return Amazon Canada product id. optimization and update of the doc

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A javascript library to extract content from other websites. Link to full size p
 ** Get fullsize from thumbs alternatives images on Amazon Canada when on a product page
 
 `Amazonca.getthumbs`
+
+** Get product Amazon Canada Id
+`Amazonca.product_id`    
+
+** Get Amazon Canada product title (Without Amazon mention)
+`Amazonca.title`
+
+** Get Amazon Canada product description (Without Amazon mention)
+`Amazonca.description`
+    
--- src/content_spoofer.js
+++ src/content_spoofer.js
@@ -12,6 +12,16 @@ window.spoof = spoof;
 
 let Amazonca = class {
   constructor() {
+    //Properties
+    this.alt_images = getthumbs;
+    this.fullsize_i_path ="/P/";
+    this.key = ".01._SX_SCRMZZZZZZZ_V";
+    this.pathname = window.location.pathname;
+    this.product_id = (pathname).substring(pathname.indexOf("product/") + 8).split("/")[0];
+    this.title = document.title.split(":")[0]
+    this.description = getMeta("description").split(":")[0]
+    
+    //Methods
     this.getthumbs = getthumbs;
   };
 
@@ -19,14 +29,12 @@ let Amazonca = class {
   getthumbs = function() {
     var t_list = new Array();
     var t_container = document.getElementById("altImages").getElementsByClassName("a-button-text");
-    var pathname = window.location.pathname;
-    var product_id = (pathname).substring(pathname.indexOf("product/") + 8).split("/")[0]
 
     for (let t_index = 0; t_index < t_container.length; t_index++) {
       images_list = t_container[t_index].getElementsByTagName("img")
       if (t_index >= 1 && images_list.length >= 1) {
         t_url = images_list[0].src.split("/");
-        fullsize_url = t_url[0] + "//" + t_url[2] + "/" + t_url[3] + "/P/" + product_id + ".01._SX_SCRMZZZZZZZ_V" + t_url[5];
+        fullsize_url = t_url[0] + "//" + t_url[2] + "/" + t_url[3] + fullsize_i_path + product_id + key + t_url[5];
         t_list.push(fullsize_url);
       }
     }
